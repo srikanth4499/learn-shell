12bdf3e00ee6e14f73418d3d6057bc66eb83e68e
srikanth4499 <srikanthreddyv4499@gmail.com>
2023-01-05 11:41:32 +0530

    shell script add line

---
+++ 03-variable.sh
@@ -0,0 +1,17 @@
+## If we assign a name to a set of data is called as variable.
+
+trainer=Raghu
+class=DevOps
+
+echo Trainer Name - $trainer
+echo Class Name - ${class}
+
+#DATE=2023-01-03
+DATE=$(date +%F)
+echo Hey, Today date is $DATE
+
+ADD=$((2+3+4/5))
+echo ADD - $ADD
+
+## Variable from command line
+echo b - $b
